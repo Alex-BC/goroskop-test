ec968bfca53876543e4cca201c6e4d5401adac33
Alexandr Chepak <djalexbc@gmail.com>
2022-06-20 13:51:34 +0300

    test

--- index.js
+++ index.js
@@ -31,6 +31,7 @@ const questions = [
 const headerContainer = document.querySelector("#header");
 const mainContainer = document.querySelector("#main");
 const listContainer = document.querySelector("#list");
+const TestContainer = document.querySelector("#test");
 
 const submitBtn = document.querySelector("#submit");
 
@@ -52,6 +53,7 @@ function clearPage() {
 function clearAll() {
   headerContainer.innerHTML = "";
   mainContainer.innerHTML = "";
+  listContainer.innerHTML = "";
 }
 
 function showQuestion() {
@@ -108,12 +110,69 @@ function nextQuestion() {
   submitBtn.hidden = true;
   if (questionIndex !== questions.length - 1) {
     console.log("Це не останнє питання");
-
     questionIndex++;
-
+    // console.log(questionIndex);
+    // for (let i = 0; i < questionIndex; i += 1) {
+    //   console.log(i);
+    // }
     clearAll();
     showQuestion();
   } else {
     console.log("Це останнє питання");
   }
+  // if (questionIndex === 1) {
+  //   console.log("e");
+  // }
+  // console.log(questionIndex);
 }
+
+// for (let i = 0; i < max; i += 1) {
+//   console.log(i);
+// }
+
+// Progress Bar
+// const dom = {
+//   progress: {
+//     progressFill: document.getElementById("progress-fill"),
+//   },
+// };
+
+// console.log(dom);
+
+//Progress Bar 2
+
+// const barList = document.getElementsByClassName("progress-bar");
+
+// function init() {
+//   for (let i = 0; i < barList.length; i++) {
+//     const limit = barList[i].getAttribute("data-limit"),
+//       color = barList[i].getAttribute("data-color");
+
+//     const innerBlock = document.createElement("div");
+
+//     innerBlock.setAttribute("data-limit", limit);
+//     innerBlock.style.backgroundColor = color;
+
+//     barList[i].appendChild(innerBlock);
+//   }
+// }
+
+// async function fillUp() {
+//   for (let i = 0; i < barList.length; i++) {
+//     let counter = 0;
+//     const result = new Promise((resolve) => {
+//       let intervalId = setInterval(() => {
+//         if (counter === +barList[i].getAttribute("data-limit")) {
+//           clearInterval(intervalId);
+//           resolve("done");
+//         }
+//         barList[i].childNodes[0].style.width = `${counter}%`;
+//         counter++;
+//       }, 15);
+//     });
+//     await result;
+//   }
+// }
+
+// init();
+// fillUp();
